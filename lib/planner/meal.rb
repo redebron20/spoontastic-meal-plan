a16f9048a7ee93093d089cb34f359f0f02165219
redebron20 <redebron20@gmail.com>
2020-12-09 00:26:14 -0500

    Build structure for class Meal

--- lib/planner/meal.rb
+++ lib/planner/meal.rb
@@ -2,6 +2,11 @@ class SpoontasticMealPlan::MealPlan
 
     @@all = []
 
+    def initialize(recipe_hash)
+        recipe_hash.each {|k, v| self.send(("#{k}="), v)}
+        save
+    end
+
     def save
         self.class.all < self
     end
@@ -9,5 +14,24 @@ class SpoontasticMealPlan::MealPlan
     def self.all
         @@all
     end
+    
+    def self.create_from_collection(recipes_arr)
+        recipes_arr.each do |recipe_hash|
+        self.new(recipe_hash)
+        end
+    end
+    
+    def add_instruction(instruction)
+        @instruction = instruction
+    end
+    
+    def add_ingredients(ingredients)
+        @ingredients = ingredients
+    end
+
+    def self.reset
+        @@all.clear
+    end 
+    
 
 end
