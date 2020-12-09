5607b195b7a3ef2b726f700734666564279b4da8
redebron20 <redebron20@gmail.com>
2020-12-09 01:47:54 -0500

    Add structure to class Ingredient

---
+++ lib/planner/ingredient.rb
@@ -0,0 +1,21 @@
+
+class SpoontasticMealPlan::Ingredient
+    attr_accessor :name, :amount, :unit
+    @@all = []
+
+    def initialize(ingredients_hash)
+        ingredients_hash.each {|k, v| self.send(("#{k}="), v)}
+        @@all << self
+    end
+
+    def self.create_from_collection(ingredients_arr)
+        ingredients_arr.each do |ingredients_hash|
+        self.new(ingredients_hash)
+        end
+    end
+
+    def self.all
+        @@all
+    end
+
+end
