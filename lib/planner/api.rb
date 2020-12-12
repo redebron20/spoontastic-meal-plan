050570935c75649755a2e8f2afea0f7c990ca537
redebron20 <redebron20@gmail.com>
2020-12-12 08:39:57 -0500

    Build API class method to retrieve meal ingredient for the user

--- lib/planner/api.rb
+++ lib/planner/api.rb
@@ -10,13 +10,17 @@ class SpoontasticMealPlan::API
     #     uri = URI(url)
     #     response = Net::HTTP.get(uri)
     #     formatted_resp = JSON.parse(response)
+
+    #     mealplan["meals"].each do |meals_hash|
+    #         SpoontasticMealPlan::Meal.new({id: meals_hash["id"].to_i, title: meals_hash["title"], servings: meals_hash["servings"], readyinMinutes: meals_hash["readyInMinutes"]})
+    #     end
     # end
 
-    KEY = "85064a6e000745df8969cd158fcf80eb"
+    # KEY = "85064a6e000745df8969cd158fcf80eb"
 
     def self.get_mealplan_day(search_hash)
         parsed_params = parse_search(search_hash)
-        url = "https://api.spoonacular.com/mealplanner/generate?timeFrame=day&diet=#{parsed_params[:diet]}&intolerances=#{parsed_params[:intolerance]}&instructionsRequired=true&apiKey=#{KEY}"
+        url = "https://api.spoonacular.com/mealplanner/generate?timeFrame=day&diet=#{parsed_params[:diet]}&intolerances=#{parsed_params[:intolerance]}&instructionsRequired=true&apiKey=#{ENV['API_KEY']}"
         uri = URI(url)
         response = Net::HTTP.get(uri)
         mealplan = JSON.parse(response)
@@ -31,6 +35,15 @@ class SpoontasticMealPlan::API
         search_hash
     end
 
+    def self.get_meal_ingredients(recipe.id)
+        binding.pry
+        url = "https://api.spoonacular.com/recipes/#{id}/information?includeNutrition=false&apiKey=#{ENV['API_KEY']}"
+        uri = URI(url)
+        response = Net::HTTP.get(uri)
+        ingredient = JSON.parse(response)
+        ingredient.map do |ingredient_hash|
+            ingredient_hash.select { |k, v| k == "name" || k == "amount" || k == "unit"}
+    end
 
 
 end
